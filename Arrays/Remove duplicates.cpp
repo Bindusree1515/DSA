d95d0e9355e132ea312532c0663a9e0f39946cec
Bindusree1515 <91887086+Bindusree1515@users.noreply.github.com>
2024-02-20 21:13:49 +0530

    Create Remove duplicates.cpp

---
+++ Arrays/Remove duplicates.cpp
@@ -0,0 +1,15 @@
+//two pointer approach
+//similarly as move zeros to end
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int j = 0;
+        for ( int i = 1 ; i < nums.size() ; i++){
+            if (nums[j]!=nums[i]){
+                nums[j+1]=nums[i];
+                j++;
+            }
+        }
+        return j+1;
+    }
+};
