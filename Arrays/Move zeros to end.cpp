18920af2fc5175c581b8ce7bbd275b6b805de10d
Bindusree1515 <91887086+Bindusree1515@users.noreply.github.com>
2024-02-20 21:12:45 +0530

    Create Move zeros to end.cpp

---
+++ Arrays/Move zeros to end.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j =0,s1=nums.size();
+        for(int i = 0 ; i<nums.size();i++){
+            if(nums[i]!=0){
+                nums[j]=nums[i];
+                j++; 
+            }
+        }
+        // 0 1 0 3 12
+        // 0 1 2 3  4 indexes
+        // 1 3 12 0 0
+
+        // int s2=nums.size(); for(i=s2 is wrong)
+        for(int i = j ; i<nums.size();i++){
+            nums[i]=0;
+        }
+    }
+};
